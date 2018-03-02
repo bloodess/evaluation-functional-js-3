751d0f06c0749fd7df266eb42b5cb402af93f143
Bloodess <arnaud.champleboux@gmail.com>
2018-03-02 10:11:17 +0100

    Exo1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,10 +1,36 @@
 const moment = require("moment");
 
 const sortProduct = input => {
-  // Your future job begins here ...
+
+ return input
+ .filter(produit => moment(produit.dateAdded).year() > 2000)
+ .sort(produit => !produit.dateAdded)
+ .reduce((acc, produit) => {
+
+   var date = moment(produit.dateAdded);
+
+   acc[produit.name] = [{ name: produit.name, 
+                          year: date.year(), 
+                          monthOfYear: date.month() + 1, 
+                          quantity: date.quantity 
+                        }];
+
+   return acc;
+ }, {});
 };
 
 module.exports = {
-  title: "Exercise 1",
-  run: sortProduct
+ title: "Exercise 1",
+ run: sortProduct
 };
+//{ banana: [ { name: 'banana', year: 2018, monthOfYear: 2, quantity: 4 } ] }
+//{ tomato: [{ name: "tomato", year: 2017, monthOfYear: 2, quantity: 3 }]}
+
+// {
+//   name: "tomato",
+//   dateAdded: 1486671098000, // 2017-02-09
+//   quantity: 3
+// }
+
+// The function should filter product before year 2000, sort by date and group by product.
+
